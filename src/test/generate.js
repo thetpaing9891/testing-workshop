f8dabf67e89ae2cae185ae43bf3b4b41cecc3fb6
Thet Paing <thetpaingsoe>
2023-09-21 13:33:28 +0630

    feat: added integration test

--- src/test/generate.js
+++ src/test/generate.js
@@ -1,25 +1,8 @@
-import faker from 'faker';
-
-function buildArtWork(overrides) {
+import * as artWorkDB from './data/artworks';
+async function buildArtWork(id = 7122) {
+  const data = await artWorkDB.read(id);
   return {
-    id: faker.datatype.number(5),
-    title: faker.lorem.words(),
-    date_start: 1600,
-    date_end: 1610,
-    date_display: faker.lorem.words(),
-    artist_display: faker.lorem.words(),
-    artist_title: faker.lorem.words(),
-    place_of_origin: faker.lorem.words(),
-    credit_line: faker.lorem.words(),
-    material_titles: [
-      'paper (fiber product)',
-      'ink',
-      'coating (material)',
-      'iron gall ink',
-    ],
-    image_id: '9484feef-7c7f-e70f-4d5e-4320ece4bfd1',
-    ...overrides,
+    ...data,
   };
 }
-
 export { buildArtWork };
